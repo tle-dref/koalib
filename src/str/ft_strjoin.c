c8ac21d1b324fea90687470cf0b307f65723e8c4
Blackbill1 <thomas.ledref@gmail.com>
2024-11-05 05:59:36 +0100

    add better join

--- src/str/ft_strjoin.c
+++ src/str/ft_strjoin.c
@@ -6,24 +6,26 @@
 /*   By: tle-dref <tle-dref@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 11:45:53 by tle-dref          #+#    #+#             */
-/*   Updated: 2024/11/04 20:26:33 by tle-dref         ###   ########.fr       */
+/*   Updated: 2024/11/05 05:58:21 by tle-dref         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *str1, char const *str2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*result;
 	size_t	len1;
 	size_t	len2;
 
-	len1 = ft_strlen(str1);
-	len2 = ft_strlen(str2);
+	if (!s1 || !s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
 	result = malloc((len1 + len2 + 1) * sizeof(char));
 	if (!result)
 		return (NULL);
-	ft_strlcpy(result, str1, len1 + 1);
-	ft_strlcat(result, str2, len1 + len2 + 1);
+	ft_strlcpy(result, s1, len1 + 1);
+	ft_strlcat(result, s2, len1 + len2 + 1);
 	return (result);
 }
