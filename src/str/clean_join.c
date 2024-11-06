eb911fc806a609b014639c74a565822944d3fa63
Blackbill1 <thomas.ledref@gmail.com>
2024-11-06 02:49:35 +0100

    clean join norm

--- src/str/clean_join.c
+++ src/str/clean_join.c
@@ -6,17 +6,17 @@
 /*   By: tle-dref <tle-dref@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 04:58:29 by tle-dref          #+#    #+#             */
-/*   Updated: 2024/11/05 04:58:57 by tle-dref         ###   ########.fr       */
+/*   Updated: 2024/11/06 02:49:13 by tle-dref         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *clean_join(char *s1, char *s2)
+char	*clean_join(char *s1, char *s2)
 {
 	char	*res;
 
 	res = ft_strjoin(s1, s2);
 	free(s1);
-	return res;
+	return (res);
 }
